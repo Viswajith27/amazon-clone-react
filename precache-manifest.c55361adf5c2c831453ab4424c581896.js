e382bd3c9ddbf4487f8c99ff3d4d743cea1932e5
Viswajith Rajan <viswajithrajan@gmail.com>
2023-09-07 20:41:01 +0530

    Updates

---
+++ precache-manifest.c55361adf5c2c831453ab4424c581896.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd0933e49cb0d9974f89eeee959bfaa",
+    "url": "/amazon-clone-react/index.html"
+  },
+  {
+    "revision": "821826c49bea249b7d5a",
+    "url": "/amazon-clone-react/static/css/main.2ca93671.chunk.css"
+  },
+  {
+    "revision": "d8b08190eb094a03d42c",
+    "url": "/amazon-clone-react/static/js/2.cfa26702.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/amazon-clone-react/static/js/2.cfa26702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821826c49bea249b7d5a",
+    "url": "/amazon-clone-react/static/js/main.0ca51c7a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/amazon-clone-react/static/js/main.0ca51c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a0114f3539c72227d21",
+    "url": "/amazon-clone-react/static/js/runtime-main.f0330b90.js"
+  }
+]);
